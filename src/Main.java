2cd6cd6a9301a2e745cf56c2a6c364d808a8613f
BeepBoopRobot <benlangford10@gmail.com>
2018-11-03 19:40:06 +0000

    i

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import javafx.application.Platform;
+import javafx.embed.swing.JFXPanel;
+import javafx.stage.Stage;
+
 public class Main {
     public static void main(String[] args) {
-
+        new JFXPanel();
+        Platform.runLater(Main::launch);
+    }
+    public int windowWidth = 1000;
+    public int windowHeight = 
+    private static void launch() {
+        Stage stage = new Stage();
+        stage.setTitle("Doppler Effect Visualisation");
     }
 }
