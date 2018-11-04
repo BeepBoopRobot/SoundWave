dc10d4ca769ad566f6ff5a7a2206d926552cfaf3
BeepBoopRobot <benlangford10@gmail.com>
2018-11-04 00:30:50 +0000

    lotta work

--- src/Emitter.java
+++ src/Emitter.java
@@ -1,15 +1,50 @@
-public class Emitter {
-    public Emitter(double speed, double freq, int rings) {
-        this.speed = speed;
-        this.freq = freq;
-        this.rings = rings;
-        this.pos = 0;
-        this.atEdge = false;
-    }
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 
+import java.util.ArrayList;
+
+public class Emitter {
     private double freq;
     private int rings;
     private int pos;
     private boolean atEdge;
     private double speed;
+    private ArrayList<Ring> waves;
+    private int maxRadius;
+    private Image image;
+
+    public Emitter(double speed, double freq, int rings, int maxRadius) {
+        this.speed = speed;
+        this.maxRadius = maxRadius;
+        this.freq = freq;
+        this.rings = rings;
+        this.pos = 0;
+        this.atEdge = false;
+        this.waves = new ArrayList<>();
+        this.image = new Image("/tie.png");
+    }
+
+    void generateRings() {
+        for(int i = 0; i<rings; i++) {
+            waves.add(new Ring(((double)i/rings) * maxRadius, 0));
+        }
+    }
+
+    public void draw(GraphicsContext gc, int h) {
+        gc.drawImage(image, pos+40, h/2-60, 120, 120);
+        for(Ring r : waves) {
+            gc.strokeOval(100-r.getRadius()/2, h/2-r.getRadius()/2, r.getRadius(), r.getRadius());
+        }
+    }
+
+    public void update() {
+        pos += speed;
+        for(Ring r : waves) {
+            if(r.getRadius() >= maxRadius) {
+                r.reset(pos);
+            } else {
+                r.update();
+            }
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,18 @@
 import javafx.animation.AnimationTimer;
 import javafx.application.Platform;
 import javafx.embed.swing.JFXPanel;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.text.DecimalFormat;
 
@@ -16,6 +20,7 @@ public class Main {
     public static void main(String[] args) {
         new JFXPanel();
         Platform.runLater(Main::launch);
+        Platform.runLater(Main::control);
     }
 
     public static int windowWidth = 1000;
@@ -23,6 +28,32 @@ public class Main {
 
     private static void close() {
         System.exit(0);
+    }
+
+    private static void control() {
+        Stage stage = new Stage();
+        stage.setTitle("Controller");
+        stage.setWidth(200);
+        stage.setHeight(500);
+        stage.setResizable(false);
+        stage.setOnCloseRequest(we -> close());
+        stage.getIcons().add(new Image("/tie.png"));
+        stage.show();
+
+        Group group = new Group();
+        Scene scene = new Scene(group);
+        stage.setScene(scene);
+
+        VBox vb = new VBox();
+        vb.setPrefWidth(200);
+        vb.setPrefHeight(500);
+        vb.setSpacing(10);
+        vb.setAlignment(Pos.CENTER);
+        group.getChildren().add(vb);
+
+        Button a = new Button("Nothing here yet");
+
+        vb.getChildren().add(a);
     }
 
     private static void launch() {
@@ -33,6 +64,7 @@ public class Main {
         stage.setResizable(false);
         stage.setOnCloseRequest(we -> close());
         stage.getIcons().add(new Image("/tie.png"));
+        stage.initStyle(StageStyle.UNDECORATED);
         stage.show();
 
         Group group = new Group();
@@ -49,17 +81,25 @@ public class Main {
         gc.setStroke(Color.BLACK);
         gc.setFill(Color.GRAY);
         gc.setFont(new Font("Lucida Sans", 24));
-        Emitter e = new Emitter(2, 20, 30);
-        AnimationTimer at = new AnimationTimer() {
-          private long last = 0;
 
-          @Override
-          public void handle(long now){
-            if(now-last >= 16_000_000) {
-                gc.fillRect(0,0,windowWidth,windowHeight);
+        Emitter e = new Emitter(2, 20, 30, 500);
+        e.generateRings();
+
+
+        AnimationTimer at = new AnimationTimer() {
+            private long last = 0;
+
+            @Override
+            public void handle(long now) {
+                if (now - last >= 16_000_000) {
+                    gc.fillRect(0, 0, windowWidth, windowHeight);
+                    e.draw(gc, windowHeight);
+                    e.update();
+                }
             }
-          }
         };
+
+        at.start();
 
     }
 }
